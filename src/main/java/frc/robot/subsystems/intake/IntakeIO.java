bd90a5905d4052a2138e56ee1686dab50c32373a
hamidh-2k8 <hamidh.2k8@gmail.com>
2026-01-24 11:48:54 -0500

    Added IntakeIO, changed WPILib version.

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -0,0 +1,40 @@
+package frc.robot.subsystems.intake;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface IntakeIO {
+    @AutoLog
+    public static class IntakeIOInputs {
+        public double velocityRadPerSec = 0.0;
+        public double appliedVolts = 0.0;
+        public double currentAmps = 0.0;
+    }
+    
+    
+    /** Updates the set of loggable inputs. */
+    public void updateInputs(IntakeIOInputs inputs);
+    
+    /**
+     * Set the voltage for the motor(s) that drives the intake wheels
+     * @param volts The voltage to set the motor to.
+     */
+    public void setWheelMotorVoltage(double volts);
+
+    /**
+     * Set the voltage for the motor(s) that drop down the intake (and shifts the hopper)
+     * @param volts The voltage to set the motor to.
+     */
+    public void setDropperMotorVoltage(double volts);
+    
+    /**
+     * Sets brakemode for the motor(s) that drives the intake wheels
+     * @param brake Whether or not to brake the motor.
+     */
+    public void setWheelBrakeMode(boolean brake);
+
+    /**
+     * Sets brakemode for the motor(s) that drops down the intake (and shifts the hopper)
+     * @param brake Whether or not to brake the motor.
+     */
+    public void setDropperBrakeMode(boolean brake);
+}
